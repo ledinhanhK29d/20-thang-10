4d27efca6d4d8a8113a5a8c7ab4001601f77c208
ledinhanhK29d <112268916+ledinhanhK29d@users.noreply.github.com>
2025-10-20 22:49:05 +0700

    Add files via upload

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mệ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng mẹ';
+const contentCard = 'Chúc mẹ 20/10 thật tươi như hoa, rực rỡ như nắng và ngọt ngào như bánh kem! Cảm ơn mẹ vì đã luôn là “siêu nhân” của con – người có thể làm mọi thứ mà vẫn xinh đẹp rạng ngời! 🦸‍♀️ Con chúc mẹ luôn khỏe mạnh, cười thật nhiều, và ngày nào cũng vui như hôm nay nhé! Con yêu mẹ nhất trên đời! 💖🌷';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
